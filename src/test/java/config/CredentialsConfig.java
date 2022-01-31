57a926c25e8510df800db6c18b697e326d1a29f7
Alexander Ignatenko <lainqa63@gmail.com>
2022-02-01 00:40:27 +0400

    Added owner homework

--- src/test/java/config/CredentialsConfig.java
+++ src/test/java/config/CredentialsConfig.java
@@ -3,7 +3,7 @@ package config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources({"resources/config/credentials.properties"})
+@Config.Sources({"src/test/resources/config/credentials.properties"})
 public interface CredentialsConfig extends Config {
     String login();
     String password();
