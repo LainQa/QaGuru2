01d70a4cc78aeb5a1527c7c9dd82475f149d2448
Alexander Ignatenko <lainqa63@gmail.com>
2022-02-01 09:37:49 +0400

    Added owner homework

--- src/test/java/simpleTest/properies/ownersTest.java
+++ src/test/java/simpleTest/properies/ownersTest.java
@@ -3,9 +3,11 @@ package simpleTest.properies;
 
 import config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static java.lang.String.format;
 
 @Tag("properties")
@@ -14,15 +16,19 @@ public class ownersTest {
             ConfigFactory.create(CredentialsConfig.class);
 
     @Test
-    void readCredentialsTest() {
-        String login = credentials.login();
-        String password = credentials.password();
-
-        System.out.println(login);
-        System.out.println(password);
-
-        String message = format("i login as %s with password %s", login, password);
-        System.out.println(message);
+    @DisplayName("Вывод на экран логина и пароля")
+    void readCredentialsTest2() {
+        step("Читаем данные из файла", () -> {
+            String login = credentials.login();
+            String password = credentials.password();
+            step("Выводим, что прочитали", () -> {
+                System.out.println(login);
+                System.out.println(password);
+                String message = format("i login as %s with password %s", login, password);
+                System.out.println(message);
+            });
+        });
     }
 }
+
 
